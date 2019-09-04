1c1a630b01f182601203ef625192a3af2171ff08
gediont <teshomegedion@gmail.com>
2019-09-04 12:22:11 +0300

    Working Module

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-module.exports = function replace(string) => {
+module.exports = function replace(string) {
 
 	return string.replace(/[aeiou]/ig, ''); // the i is caseinsenstive and g is global
 }
 
-
-
-// let findAndReplace = (string) => {
-
-// 	string.split('');
-	
-// 	for(var i=0; i<string.length; i++){
-// 		if(string[i] == 'a' || string[i] ==  'A' || string[i] ==  'e' || string[i] ==  'E' || string[i] ==  'i' || string[i] == 'o' || string[i] ==  'O' || string[i] ==  'u' || string[i] ==  'U' )
-// 		{
-// 			string[i] == ' ';
-// 		}
-// 	}
-// 		 return string;
-// }
-
-// console.log(findAndReplace('Gedion'));
